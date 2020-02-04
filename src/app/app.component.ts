7b1dfa3aef70b28d87776023b004820ecda3ed3f
Sabrina Reichel <s0553282@htw-berlin.de>
2020-02-04 19:54:39 +0100

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontEND';
+
+  //später durch Datenbank
+  zettels = ["1. Zettel","2",3,4,5,6,7,8,9,10, 11,12,13,14,15,16, 17, 18, 19]
+
+ 
+  getRandomColor() {
+    var color = Math.floor(0x1000000 * Math.random()).toString(16);
+    return '#' + ('000000' + color).slice(-6);
+  }
 }
